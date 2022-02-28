052dbc8adb535b7d0ba0066633b38aaa1940de90
iaht <klrt0120@gmail.com>
2022-02-28 10:55:24 +0700

    Demo SASS : bird animation

---
+++ src/Component/header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../header/style.scss';
+export default function Header() {
+  return (
+    <div>
+      <div className="header">
+       <div className="header-inner"> 
+            <div className="header-inner-text">
+              <p>Lorem</p>
+            </div>
+       </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/animation/Animation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./animation.scss";
+export default function Animation() {
+  return (
+    <div className="bird">
+      <div className="container">
+        <div className="body"> </div>
+        <div className="mouth"></div>
+        <div className="beak"></div>
+        <div className="feather"></div>
+        <div className="tail"></div>
+        <div className="leg"></div>
+        <div className="brand">
+          <div className="b"> _</div>
+          <div className="b"> B</div>
+          <div className="i"> I</div>
+          <div className="r"> R</div>
+          <div className="d"> D</div>
+        </div>
+      </div>
+    </div>
+  );
+}
